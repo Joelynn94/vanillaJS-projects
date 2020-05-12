ec1e0acaea56a41afd20cf1e2327696b532e6bc2
Joseph Lynn <josephlynn94@gmail.com>
2020-05-12 07:58:12 -0400

    added folder structure, files and html/css content for the modal and landing page

---
+++ menu-slider-modal/index.js
@@ -0,0 +1,4 @@
+const toggle = document.querySelector('#toggle');
+const close = document.querySelector('#close');
+const open = document.querySelector('#open');
+const modal = document.querySelector('#modal');
